0eec111f7affe4309039db5977a95c3f9d8e3066
Alex Lepadatu <allepadatu@gmail.com>
2025-07-24 19:40:38 +0300

    test

--- app/src/main/java/com/example/hackaton2025/MainActivity.kt
+++ app/src/main/java/com/example/hackaton2025/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        
+
         setContent {
             Hackaton2025Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
