816f1d433131ee2aa7c8038c121781fd787c2abb
Alex Lepadatu <allepadatu@gmail.com>
2025-07-25 04:59:54 +0300

    pooling

--- app/src/main/java/com/example/hackaton2025/data/repository/EducationRepository.kt
+++ app/src/main/java/com/example/hackaton2025/data/repository/EducationRepository.kt
@@ -3,8 +3,11 @@ package com.example.hackaton2025.data.repository
 import android.util.Log
 import com.example.hackaton2025.data.api.UserApi
 import com.example.hackaton2025.data.model.UserDto
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.isActive
 import javax.inject.Inject
 
 interface EducationRepository {
@@ -14,14 +17,20 @@ interface EducationRepository {
 class EducationRepositoryImpl @Inject constructor(private val api: UserApi) : EducationRepository {
     override fun getUsers(): Flow<List<UserDto>> {
         return flow {
-            val response = api.getPostsForUser()
+            coroutineScope {
+                while (isActive) {
+                    val response = api.getPostsForUser()
 
-            if (response.isSuccessful)
-                response.body()?.let {
-                    emit(it)
+                    if (response.isSuccessful)
+                        response.body()?.let {
+                            emit(it)
+                        }
+                    else
+                        Log.e("repo", "fauled to get: ${response.errorBody()?.string()}")
+
+                    delay(10_000)
                 }
-            else
-                Log.e("repo", "fauled to get: ${response.errorBody()?.string()}")
+            }
         }
     }
 }
